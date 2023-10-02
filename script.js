cc704e80e112dd3fe4082a1e21079025e1ce7a2e
Renzo Diaz <renzodiaz554@gmail.com>
2023-10-02 08:09:17 -0500

    CommonJS Modules

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-const ShoppingCart = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
-  const addToCart = function (product, quantity) {
+// Export
+export.addToCart = function (product, quantity) {
     cart.push({ product, quantity });
     console.log(
       `${quantity} ${product} added to cart, shipping cost is ${shippingCost}`
     );
   };
-  const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
 
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
-
-ShoppingCart.addToCart('apple', 4); //Closures allow to access to the method
-ShoppingCart.addToCart('pizza', 2);
-console.log(ShoppingCart.cart);
-// console.log(ShoppingCart.shippingCost); //Undefined since this method was not exposed
+//Import
+const {addToCart} = require("./shoppingCart.js");
