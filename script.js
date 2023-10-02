1552f8a42bf2dfe44d8c07d2723802ba931585fa
Renzo Diaz <renzodiaz554@gmail.com>
2023-10-02 08:50:45 -0500

    Introduction to NPM

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-// Export
-export.addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(
-      `${quantity} ${product} added to cart, shipping cost is ${shippingCost}`
-    );
-  };
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 
-//Import
-const {addToCart} = require("./shoppingCart.js");
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+
+console.log(stateClone);
+console.log(stateDeepClone);
