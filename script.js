ab070f5a08ec85c4711713269d5c2de2c4bdb9ab
Renzo Diaz <renzodiaz554@gmail.com>
2023-10-01 23:42:43 -0500

    The Module Pattern

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-import add, { cart } from './shoppingCart.js';
-
-const getLastPost = async () => {
-  const jsonPlaceholderResponse = await fetch(
-    'https://jsonplaceholder.typicode.com/posts'
-  );
-  const jsonPlaceholderData = await jsonPlaceholderResponse.json();
+const ShoppingCart = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart, shipping cost is ${shippingCost}`
+    );
+  };
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
 
   return {
-    title: jsonPlaceholderData.at(-1).title,
-    body: jsonPlaceholderData.at(-1).body,
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
   };
-};
+})();
 
-const lastPost = await getLastPost();
-console.log(lastPost);
+ShoppingCart.addToCart('apple', 4); //Closures allow to access to the method
+ShoppingCart.addToCart('pizza', 2);
+console.log(ShoppingCart.cart);
+// console.log(ShoppingCart.shippingCost); //Undefined since this method was not exposed
