731a26eef50871ddf3b242eacc76dba7301956d8
Renzo Diaz <renzodiaz554@gmail.com>
2023-10-01 23:28:10 -0500

    Top-Level Await (ES2022)

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-/* NAMED EXPORTS */
-
-// import { addToCart, totalPrice as price, quantity } from './shoppingCart.js';
-
-console.log(`Importing module...`);
-// // console.log(shippingCost); //Not possible: variables scope is only the module itself
-
-// addToCart('bread', 5);
-// console.log(price, quantity);
-
-// import * as ShoppingCart from './shoppingCart.js';
-
-// ShoppingCart.addToCart('bread', 5);
-// console.log(ShoppingCart.totalPrice);
-
-/* DEFAULT EXPORTS */
-
 import add, { cart } from './shoppingCart.js';
 
-add('pizza', 2);
-add('bread', 5);
-add('apples', 4);
+const getLastPost = async () => {
+  const jsonPlaceholderResponse = await fetch(
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+  const jsonPlaceholderData = await jsonPlaceholderResponse.json();
 
-console.log(cart);
+  return {
+    title: jsonPlaceholderData.at(-1).title,
+    body: jsonPlaceholderData.at(-1).body,
+  };
+};
+
+const lastPost = await getLastPost();
+console.log(lastPost);
--- shoppingCart.js
+++ shoppingCart.js
@@ -1,12 +1,17 @@
 console.log(`Exporting module...`);
 
+// console.log('Start fetching users...');
+// //If this module is imported, the importer will have to wait for this line to complete
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users...');
+
 const shippingCost = 10;
 export const cart = [];
 
-// export const addToCart = (product, quantity) => {
-//   cart.push({ product, quantity });
-//   console.log(`${quantity} ${product} added to cart`);
-// };
+export const addToCart = (product, quantity) => {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
 
 const totalPrice = 237;
 const totalQuantity = 23;
