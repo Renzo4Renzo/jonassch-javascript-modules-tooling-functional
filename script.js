17455fc1883906da95f909574e68a37b239bb46b
Renzo Diaz <renzodiaz554@gmail.com>
2023-10-03 20:44:25 -0500

    Bundling With Parcel and NPM Scripts

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash';
+
+import add, { cart } from './shoppingCart.js';
+
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
 
 const state = {
   cart: [
@@ -14,3 +22,11 @@ state.user.loggedIn = false;
 
 console.log(stateClone);
 console.log(stateDeepClone);
+
+// const title = document.querySelector('.title');
+// title.innerHTML = 'Baby I love the way';
+
+/*Parcel code that prevents page-reload*/
+if (module.hot) {
+  module.hot.accept();
+}
